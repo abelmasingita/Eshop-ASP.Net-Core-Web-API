0d1ad2cf0aa7ad4ce5c9002277a5db973f4d1788
abel masingita <60102940+abelmasingita@users.noreply.github.com>
2023-02-04 00:46:55 +0200

    Initialized eshop web api

---
+++ eshopWebAPI/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿namespace eshopWebAPI.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public double Rating { get; set; }
+
+        //public ICollection Reviews { get; set; }
+        //public string User { get; set; }    
+        public int NumReviews { get; set; }
+        public int CountInStock { get; set; }
+        public string Brand { get; set; }
+        public string Category { get; set; }
+
+    }
+}
---
+++ eshopWebAPI/Models/User.cs
@@ -0,0 +1,15 @@
+﻿namespace eshopWebAPI.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+
+        public bool IsAdmin { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
