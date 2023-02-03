bda89e796a9cf28b31633fb32b2f616af35eff0b
abel masingita <60102940+abelmasingita@users.noreply.github.com>
2023-02-04 00:58:46 +0200

    added models and DbContext

---
+++ eshopWebAPI/Data/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using eshopWebAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace eshopWebAPI.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
