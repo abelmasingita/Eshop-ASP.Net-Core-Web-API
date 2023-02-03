587db0dda430e1db974b4def379acd586c76c91d
abel masingita <60102940+abelmasingita@users.noreply.github.com>
2023-02-04 01:56:58 +0200

    Added migrations and database seeder

--- eshopWebAPI/Program.cs
+++ eshopWebAPI/Program.cs
@@ -1,13 +1,39 @@
+using eshopWebAPI.Data;
+using eshopWebAPI.Seed;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddTransient<Seeder>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("eshopConnectionStr"));
+});
+
 
 var app = builder.Build();
+
+//Seed
+
+//if (args.Length == 1 && args[0].ToLower() == "seeddata")
+    SeedData(app);
+
+void SeedData(IHost app)
+{
+    var scopedFactory = app.Services.GetService<IServiceScopeFactory>();
+
+    using (var scope = scopedFactory.CreateScope())
+    {
+        var service = scope.ServiceProvider.GetService<Seeder>();
+        service.SeedData();
+    }
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
---
+++ eshopWebAPI/Seed/Seeder.cs
@@ -0,0 +1,31 @@
+﻿using eshopWebAPI.Data;
+using eshopWebAPI.Models;
+
+namespace eshopWebAPI.Seed
+{
+    public class Seeder
+    {
+        private readonly DataContext _context;
+
+        public Seeder(DataContext context)
+        {
+            _context = context;
+        }
+
+        public void SeedData()
+        {
+            if (!_context.Products.Any())
+            {
+                _context.Users.Add(new User { Name = "Abel Masingita", Email = "abelmasingita9@gmail.com", Password = "2196411223365441", Phone = "0742261505", Address = "Abel Masingita", IsAdmin=true, IsDeleted=false});
+                _context.Users.Add(new User { Name = "Abel Hlongwani", Email = "abelmasingita910@gmail.com", Password = "2196411223365442", Phone = "0742261505", Address = "Abel Masingita", IsAdmin = true, IsDeleted = false });
+                _context.Users.Add(new User { Name = "Abel Matimba", Email = "hlongwaniab@gmail.com", Password = "2196411223365443", Phone = "0742261505", Address = "Abel Masingita", IsAdmin = true, IsDeleted = false });
+
+                _context.Products.Add(new Product {Name= "Batho SKR", Description="Walk your journey", Price=100.23, Rating=5.2, NumReviews=5,CountInStock=10,Brand="Batho", Category="Sneaker Men" });
+                _context.Products.Add(new Product { Name = "Drip SKR", Description = "Walk your journey", Price = 100.23, Rating = 5.2, NumReviews = 5, CountInStock = 10, Brand = "Batho", Category = "Sneaker Men" });
+                _context.Products.Add(new Product { Name = "Vaya", Description = "Walk your journey", Price = 100.23, Rating = 5.2, NumReviews = 5, CountInStock = 10, Brand = "Batho", Category = "Sneaker Men" });
+
+                _context.SaveChanges(); 
+            }
+        }
+    }
+}
